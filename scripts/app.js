cafd0bf6f1f58e965153bbb4de19c3a1cd671a0a
vladislavpetrov4311 <petrov_vd_4311@mail.ru>
2022-05-27 12:32:21 +0300

    переработка кода с помощью функции (3 изменение 5 лаба)

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,12 @@
 var main = function () {
-	var makeTabActive = function (tabNumber) {
+	var tabNumber;
+	for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
 		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-		$(".tabs span").removeClass("active");
-		$(tabSelector).addClass("active");
-		}
-
-		$(".tabs a:nth-child(1)").on("click", function () {
-			makeTabActive(1);
+		$(tabSelector).on("click", function () {
+			$(".tabs span").removeClass("active");
+			$(this).addClass("active");
 			return false;
 		});
-		$(".tabs a:nth-child(2)").on("click", function () {
-			makeTabActive(2);
-			return false;
-		});
-		$(".tabs a:nth-child(3)").on("click", function () {
-			makeTabActive(3);
-			return false;
-
-	});
+	}
 };
 $(document).ready(main);
