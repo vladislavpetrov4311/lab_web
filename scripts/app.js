3f927b7d8715b4928f620755743700e104fcb6b5
vladislavpetrov4311 <petrov_vd_4311@mail.ru>
2022-05-27 13:17:44 +0300

    перенос массива в json 5 лаба

--- scripts/app.js
+++ scripts/app.js
@@ -1,42 +1,40 @@
 var main = function (toDoObjects) {
 	"use strict";
+	// как main имеет доступ к списку задач!
 	var toDos = toDoObjects.map(function (toDo) {
-		// просто возвращаем описание
-		// этой задачи
+		// просто возвращаем описание этой задачи
 		return toDo.description;
 	});
-	// сейчас весь старый код должен работать в точности как раньше!
-	// ...
-
-	$("document").ready(function () {
-		$(".tabs a span").toArray().forEach(function (element) {
+	// сейчас весь старый код должен работать как раньше!
+	$("document").ready( function(){
+		$(".tabs a span").toArray().forEach(function (element) { 
 			$(element).on("click", function () {
 				var $element = $(element);
-				$(".tabs a span").removeClass("active");
+				$(".tabs a span").removeClass("active"); 
 				$(element).addClass("active");
 				$("main .content").empty();
 
-				if ($element.parent().is(":nth-child(1)")) {
-					for (var i = toDos.length - 1; i > -1; i--) {
+				if ($element.parent().is(":nth-child(1)")) { 
+					for (var i = toDos.length-1; i > -1; i--) { 
 						$(".content").append($("<li>").text(toDos[i]));
 					}
-				}
-				else if ($element.parent().is(":nth-child(2)")) {
-					toDos.forEach(function (todo) {
+				} 
+				else if ($element.parent().is(":nth-child(2)")) { 
+					toDos.forEach(function (todo) { 
 						$(".content").append($("<li>").text(todo));
 					});
-				}
-				else if ($element.parent().is(":nth-child(3)")) {
+				} 
+				else if ($element.parent().is(":nth-child(3)")) { 
 					$(".content").append(
-						'<input type="text" class="inp">' +
+						'<input type="text" class="inp">'+
 						'<button class="btn">Добавить</button>'
 					);
 					var newToDo;
-					$('.btn').on('click', function () {
-						newToDo = $('.inp').val();
-						if (newToDo != '') {
-							toDos.push(newToDo);
-							alert('Новое задание "' + newToDo + '" успешно добавлено!');
+					$('.btn').on('click',function(){
+						newToDo= $('.inp').val();
+						if (newToDo!='') {
+							toDos.push( newToDo);
+							alert('Новое задание "'+newToDo+'" успешно добавлено!');
 							$('.inp').val("");
 						}
 					})
@@ -44,12 +42,13 @@ var main = function (toDoObjects) {
 				return false;
 			})
 		})
-		$(".tabs a:first-child span").trigger("click");
+	$(".tabs a:first-child span").trigger("click");
 	})
-};
+};	
+
 $(document).ready(function () {
-	$.getJSON("todos.json", function (toDoObjects) {
-		// вызываем функцию main с задачами в качестве аргумента
+	$.getJSON("./scripts/todos.json", function (toDoObjects) {
+	// вызов функции main с аргументом в виде объекта toDoObjects 
 		main(toDoObjects);
 	});
 });
