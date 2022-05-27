b26a64bb02ab34e13ce1c1c2ee075f2fd605f899
vladislavpetrov4311 <petrov_vd_4311@mail.ru>
2022-05-27 13:19:33 +0300

    добавление вкладки теги 5 лаба

--- scripts/app.js
+++ scripts/app.js
@@ -24,7 +24,13 @@ var main = function (toDoObjects) {
 						$(".content").append($("<li>").text(todo));
 					});
 				} 
+				
 				else if ($element.parent().is(":nth-child(3)")) { 
+					// ЭТО КОД ДЛЯ ВКЛАДКИ ТЕГИ
+					console.log("Щелчок на вкладке Теги");
+				}
+
+				else if ($element.parent().is(":nth-child(4)")) { 
 					$(".content").append(
 						'<input type="text" class="inp">'+
 						'<button class="btn">Добавить</button>'
