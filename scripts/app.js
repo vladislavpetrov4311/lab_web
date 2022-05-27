3c27edb9855ffe10dde4436c3082e3d090e98374
vladislavpetrov4311 <petrov_vd_4311@mail.ru>
2022-05-27 13:21:02 +0300

    создание пользовательского интерфейса

--- scripts/app.js
+++ scripts/app.js
@@ -28,6 +28,47 @@ var main = function (toDoObjects) {
 				else if ($element.parent().is(":nth-child(3)")) { 
 					// ЭТО КОД ДЛЯ ВКЛАДКИ ТЕГИ
 					console.log("Щелчок на вкладке Теги");
+
+					var organizedByTag = 
+					[
+					{
+					"name": "покупки",
+					"toDos": ["Купить продукты"]
+					},
+					{
+					"name": "рутина",
+					"toDos": ["Купить продукты", "Вывести Грейси на прогулку в парк"]
+					},
+					{
+					"name": "писательство",
+					"toDos": ["Сделать несколько новых задач", "Закончить писать книгу"]
+					},
+					{
+					"name": "работа",
+					"toDos": ["Сделать несколько новых задач", "Подготовиться к лекции в понедельник",
+					"Ответить на электронные письма", "Закончить писать книгу"]
+					},
+					{
+					"name": " преподавание",
+					"toDos": ["Подготовиться к лекции в понедельник"]
+					},
+					{
+					"name": "питомцы",
+					"toDos": ["Вывести Грейси на прогулку в парк "]
+					}
+					]
+
+					organizedByTag.forEach(function (tag) { 
+						var $tagName = $("<h3>").text(tag.name), 
+						$content = $("<ul>"); 
+						tag.toDos.forEach(function (description) { 
+							var $li = $("<li>").text(description); 
+							$content.append($li);
+						});
+						$("main .content").append($tagName); 
+						$("main .content").append($content);
+					});
+
 				}
 
 				else if ($element.parent().is(":nth-child(4)")) { 
