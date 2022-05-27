792f3036f28d86bfbb6f55c4a833975cfdc3e883
vladislavpetrov4311 <petrov_vd_4311@mail.ru>
2022-05-27 12:30:29 +0300

    преобразование кода с помощью функции 5 лаба

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,22 @@
 var main = function () {
-
-	$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(1)").on("click", function () {	
-		$(".tabs span").removeClass("active");		
-		$(".tabs a:nth-child(1) span").addClass("active");	
-		$("main .content").empty();	
-		return false;
-	});
-
-	$(".tabs a:nth-child(2)").on("click", function () {
+	var makeTabActive = function (tabNumber) {
+		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
 		$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(2) span").addClass("active");
-		$("main .content").empty();
-		return false;
-	});
+		$(tabSelector).addClass("active");
+		}
 
-	$(".tabs a:nth-child(3)").on("click", function () {
-		$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(3) span").addClass("active");
-		$("main .content").empty();
-		return false;
+		$(".tabs a:nth-child(1)").on("click", function () {
+			makeTabActive(1);
+			return false;
+		});
+		$(".tabs a:nth-child(2)").on("click", function () {
+			makeTabActive(2);
+			return false;
+		});
+		$(".tabs a:nth-child(3)").on("click", function () {
+			makeTabActive(3);
+			return false;
+
 	});
 };
 $(document).ready(main);
