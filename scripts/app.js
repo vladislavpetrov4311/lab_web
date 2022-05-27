6349da19ed447985d249ab3375ed49e11a3c460c
vladislavpetrov4311 <petrov_vd_4311@mail.ru>
2022-05-27 12:39:35 +0300

    управление содержимым ( изменение в  5 лабе)

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,19 @@
-var main = function () {
+$("document").ready( function() {
+
 	$(".tabs a span").toArray().forEach(function (element) {
 		$(element).on("click", function () {
+			var $element = $(element);
 			$(".tabs a span").removeClass("active");
-			$(element).addClass("active");
+			$element.addClass("active");
 			$("main .content").empty();
+			if ($element.parent().is(":nth-child(1)")) {
+				console.log("Щелчок на первой вкладке!");
+			} else if ($element.parent().is(":nth-child(2)")) {
+				console.log("Щелчок на второй вкладке!");
+			} else if ($element.parent().is(":nth-child(3)")) {
+				console.log("Щелчок на третьей вкладке!");
+			}
 			return false;
 		});
 	});
-};
-$(document).ready(main);
+});
