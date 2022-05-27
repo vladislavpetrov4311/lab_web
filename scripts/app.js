39ca3172a44bc6b01473366a57c21a2373e013a7
vladislavpetrov4311 <petrov_vd_4311@mail.ru>
2022-05-27 12:29:00 +0300

    добовление переключения вкладок (2 изменение 5 лаба)

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,24 @@
 var main = function () {
-	console.log("hello, world!");
+
+	$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(1)").on("click", function () {	
+		$(".tabs span").removeClass("active");		
+		$(".tabs a:nth-child(1) span").addClass("active");	
+		$("main .content").empty();	
+		return false;
+	});
+
+	$(".tabs a:nth-child(2)").on("click", function () {
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(2) span").addClass("active");
+		$("main .content").empty();
+		return false;
+	});
+
+	$(".tabs a:nth-child(3)").on("click", function () {
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(3) span").addClass("active");
+		$("main .content").empty();
+		return false;
+	});
 };
 $(document).ready(main);
